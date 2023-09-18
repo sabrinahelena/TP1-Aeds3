8d9a139855abb54b261b4b544ec4b6c26d6860f6
sabrina helena <sabrina.ferreira@me.com.br>
2023-09-18 20:17:10 -0300

    removendo codigo unitilizado

--- src/Main.java
+++ src/Main.java
@@ -15,59 +15,14 @@ public class    Main {
     public static void main(String[] args) throws IOException {
         // Ler os dados do CSV
         List<SchoolShooting> schoolShootingData = readCSV();
-        /*
-        SchoolShooting schoolShootingRead = getSchool(schoolShootingData, 3);
 
-        if (schoolShootingRead != null) {
-            System.out.println("Registro lido: " + schoolShootingRead);
-        } else {
-            System.out.println("Registro não encontrado.");
-        }
-
-         */
 
         SchoolShooting schoolShooting = new SchoolShooting();
         schoolShooting.ArmazenarEmArquivoDb(schoolShootingData, DB_FILE_PATH);
 
-       // createAndWriteSchoolData(schoolShootingData, "Escola Teste", "Nova Localidade", "Nova Data", "Novo Ano");
 
     }
-    private static void createAndWriteSchoolData(List<SchoolShooting> schoolShootingData, String schoolName, String locality, String date, String year) {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(DB_FILE_PATH, true);
-             DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)) {
 
-            int id = generateUniqueId(schoolShootingData); // Gere um ID único
-            String idString = String.valueOf(id);
-
-
-            // Crie um novo objeto School com os dados fornecidos
-            SchoolShooting newSchoolShooting = new SchoolShooting(id, schoolName, locality, date, year);
-
-            // Escreva os dados no arquivo
-            dataOutputStream.writeUTF(idString);
-            dataOutputStream.writeUTF(newSchoolShooting.getSchoolName());
-            dataOutputStream.writeUTF(newSchoolShooting.getLocality());
-            dataOutputStream.writeUTF(newSchoolShooting.getDate());
-            dataOutputStream.writeUTF(newSchoolShooting.getYear());
-
-            // Adicione o novo objeto à lista schoolData
-            schoolShootingData.add(newSchoolShooting);
-
-            System.out.println("Registro criado e gravado: " + newSchoolShooting);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private static int generateUniqueId(List<SchoolShooting> schoolShootingData) {
-        // Implemente uma lógica para gerar um ID único, por exemplo, encontre o maior ID existente e adicione 1.
-        int maxId = 0;
-        for (SchoolShooting schoolShooting : schoolShootingData) {
-            if (schoolShooting.getId() > maxId) {
-                maxId = schoolShooting.getId();
-            }
-        }
-        return maxId + 1;
-    }
     private static List<SchoolShooting> readCSV() {
         List<SchoolShooting> schoolShootingData = new ArrayList<>();
 
@@ -102,12 +57,4 @@ public class    Main {
         return schoolShootingData;
     }
 
-    private static SchoolShooting getSchool(List<SchoolShooting> schoolShootingData, int id) {
-        for (SchoolShooting schoolShootingReadRecord : schoolShootingData) {
-            if (schoolShootingReadRecord.getId() == id) {
-                return schoolShootingReadRecord;
-            }
-        }
-        return null;
-    }
 }
